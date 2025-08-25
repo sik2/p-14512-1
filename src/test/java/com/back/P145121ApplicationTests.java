33b78282bbb75a5e34afcb48c060c910d2063704
sik2 <bickwang123@gmail.com>
2025-08-25 15:44:15 +0900

    init

---
+++ src/test/java/com/back/P145121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P145121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
