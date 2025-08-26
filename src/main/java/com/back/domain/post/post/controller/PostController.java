3d44f1c8f6ecb4931b9c648d659a02f22130b621
sik2 <bickwang123@gmail.com>
2025-08-26 11:10:53 +0900

    @Valid

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -2,20 +2,20 @@ package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
-@Validated
 public class PostController {
     private final PostService postService;
 
@@ -25,21 +25,25 @@ public class PostController {
         return getWriteFormHtml("", "", "");
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        @NotBlank
+        @Size(min = 2, max = 20)
+        String title;
+
+        @NotBlank
+        @Size(min = 2, max = 100)
+        String content;
+    }
+
     @PostMapping("/posts/doWrite")
     @ResponseBody
     @Transactional
     public String write(
-            @NotBlank
-            @Size(min = 2, max = 20)
-            @RequestParam(defaultValue = "")
-            String title,
-
-            @NotBlank
-            @Size(min = 2, max = 100)
-            @RequestParam(defaultValue = "")
-            String content
+        @Valid WriteForm writeForm
     ) {
-        Post post = postService.write(title, content);
+        Post post = postService.write(writeForm.getTitle(), writeForm.getContent());
 
         return "%d 번 글이 생성 되었습니다.".formatted(post.getId());
     }
