b39ac9fd8bb61d9ad2e708204addab98ca9b5573
sik2 <bickwang123@gmail.com>
2025-08-26 09:45:05 +0900

    form

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,11 +1,23 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
 public class PostController {
     private final PostService postService;
+
+    @PostMapping("/write")
+    @ResponseBody
+    public Post write(@RequestParam(value = "title") String title, @RequestParam(value = "content") String content) {
+        Post post = postService.write(title, content);
+
+        return post;
+    }
 }
