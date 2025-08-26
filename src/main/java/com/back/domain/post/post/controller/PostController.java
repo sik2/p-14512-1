4f8e565f1bc5a3439a203ab931788b497e652e80
sik2 <bickwang123@gmail.com>
2025-08-26 10:03:53 +0900

    write form

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -1,11 +1,9 @@
 package com.back.domain.post.post.controller;
 
-import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
@@ -13,11 +11,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PostController {
     private final PostService postService;
 
-    @PostMapping("/write")
+    @GetMapping("/posts/write")
     @ResponseBody
-    public Post write(@RequestParam(value = "title") String title, @RequestParam(value = "content") String content) {
-        Post post = postService.write(title, content);
-
-        return post;
+    public String write() {
+        return """
+                <form action="http://localhost:8080/posts/doWrite" target="_blank">
+                  <input type="text" name="title" placeholder="제목" value="안녕">
+                  <br>
+                  <textarea name="content" placeholder="내용">반가워</textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """;
     }
 }
