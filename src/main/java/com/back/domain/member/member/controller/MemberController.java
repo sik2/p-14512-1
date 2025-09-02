1b8c0994adcbd8580943b89c9356cf3d06b86240
sik2 <bickwang123@gmail.com>
2025-09-02 10:08:20 +0900

    post register

--- src/main/java/com/back/domain/member/member/controller/MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/MemberController.java
@@ -1,9 +1,18 @@
 package com.back.domain.member.member.controller;
 
 import com.back.domain.member.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
@@ -13,7 +22,38 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/register")
-    public String showRegister() {
+    public String showRegister(RegisterForm registerForm) {
         return "member/member/register";
+    }
+
+    @ToString
+    @Getter
+    @Setter
+    public static class RegisterForm{
+        @Size(min=3, max= 25)
+        @NotBlank(message = "유저 ID는 필수 항목입니다.")
+        private String username;
+
+        @NotBlank(message = "패스워드는 필수 항목입니다.")
+        private String password;
+
+        @NotBlank(message = "패스워드 확인은 필수 항목입니다.")
+        private String password_conform;
+
+        @NotBlank(message = "이메일은  필수 항목입니다.")
+        @Email
+        private String email;
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid RegisterForm registerForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "member/member/register";
+        }
+
+        System.out.println("registerForm : " + registerForm);
+        // TODO: 검증 처리
+
+        return "post/post/list";
     }
 }
--- src/main/java/com/back/domain/member/member/entity/Member.java
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.back.domain.member.member.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 public class Member extends BaseEntity {
+    @Column(unique = true)
     private String username;
     private String password;
+    @Column(unique = true)
     private String email;
 
     public Member(String username, String password, String email) {
